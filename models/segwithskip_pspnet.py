2d3f0e0f3ff1b6d91eedfc6b61ed0dd1f96a9d24
Abhishek Sen <abhisheksen1623@gmail.com>
2019-03-15 21:12:53 -0700

    tab to space

--- models/segwithskip_pspnet.py
+++ models/segwithskip_pspnet.py
@@ -38,87 +38,87 @@ class PSPUpsample(nn.Module):
 
 
 class segnetDown2(nn.Module):
-	def __init__(self, in_size, out_size):
-		super(segnetDown2, self).__init__()
-		self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-		self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-		self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+    def __init__(self, in_size, out_size):
+        super(segnetDown2, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
 
-	def forward(self, inputs):
-		outputs = self.conv1(inputs)
-		outputs = self.conv2(outputs)
-		unpooled_shape = outputs.size()
-		outputs, indices = self.maxpool_with_argmax(outputs)
-		return outputs, indices, unpooled_shape
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
 
 
 class segnetDown3(nn.Module):
-	def __init__(self, in_size, out_size):
-		super(segnetDown3, self).__init__()
-		self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-		self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-		self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-		self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+    def __init__(self, in_size, out_size):
+        super(segnetDown3, self).__init__()
+        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
 
-	def forward(self, inputs):
-		outputs = self.conv1(inputs)
-		outputs = self.conv2(outputs)
-		outputs = self.conv3(outputs)
-		unpooled_shape = outputs.size()
-		outputs, indices = self.maxpool_with_argmax(outputs)
-		return outputs, indices, unpooled_shape
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
 
 # Used in the last Decoder when skip connection is not needed
 class segnetUp1(nn.Module):
-	def __init__(self, in_size, out_size):
-		super(segnetUp1, self).__init__()
-		self.unpool = nn.MaxUnpool2d(2, 2)
-		self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
-		self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+    def __init__(self, in_size, out_size):
+        super(segnetUp1, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
 
-	def forward(self, inputs, indices, output_shape):
-		outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
-		outputs = self.conv1(outputs)
-		outputs = self.conv2(outputs)
-		return outputs
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        return outputs
 
 class segnetUp2(nn.Module):
-	def __init__(self, in_size, out_size):
-		super(segnetUp2, self).__init__()
-		self.unpool = nn.MaxUnpool2d(2, 2)
-		self.conv1 = conv2DBatchNormRelu(in_size*1.5, in_size, 3, 1, 1)
-		self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+    def __init__(self, in_size, out_size):
+        super(segnetUp2, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size*1.5, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
 
-	def forward(self, inputs, indices, output_shape, skip_con):
-		outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
-		outputs = torch.cat((outputs, skip_con), dim=1)
-		outputs = self.conv1(outputs)
-		outputs = self.conv2(outputs)
-		return outputs
+    def forward(self, inputs, indices, output_shape, skip_con):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = torch.cat((outputs, skip_con), dim=1)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        return outputs
 
 
 class segnetUp3(nn.Module):
-	def __init__(self, in_size, out_size):
-		super(segnetUp3, self).__init__()
-		self.unpool = nn.MaxUnpool2d(2, 2)
-# 		if out_size == 256:
-# 			self.conv1 = conv2DBatchNormRelu(768, in_size, 3, 1, 1)
-# 		else:
-# 		if out_size ==512:
-# 			self.conv1 = conv2DBatchNormRelu(in_size*2, in_size, 3, 1, 1)           
-# 		else:
-		self.conv1 = conv2DBatchNormRelu(in_size + out_size, in_size, 3, 1, 1)
-		self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
-		self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+    def __init__(self, in_size, out_size):
+        super(segnetUp3, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+#       if out_size == 256:
+#           self.conv1 = conv2DBatchNormRelu(768, in_size, 3, 1, 1)
+#       else:
+#       if out_size ==512:
+#           self.conv1 = conv2DBatchNormRelu(in_size*2, in_size, 3, 1, 1)           
+#       else:
+        self.conv1 = conv2DBatchNormRelu(in_size + out_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
 
-	def forward(self, inputs, indices, output_shape, skip_con):
-		outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
-		outputs = torch.cat((outputs, skip_con), dim=1)
-		outputs = self.conv1(outputs)
-		outputs = self.conv2(outputs)
-		outputs = self.conv3(outputs)
-		return outputs
-		
+    def forward(self, inputs, indices, output_shape, skip_con):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = torch.cat((outputs, skip_con), dim=1)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        return outputs
+        
 class conv2DBatchNormRelu(nn.Module):
     def __init__(
         self,
@@ -153,23 +153,23 @@ class conv2DBatchNormRelu(nn.Module):
     def forward(self, inputs):
         outputs = self.cbr_unit(inputs)
         return outputs
-		
+        
 
 class DenseSegWithSkipPSPNet(nn.Module):
 
-	def __init__(self, num_classes=15, img_channels=3, sizes=(1, 2, 3, 6), psp_size=512):
-		super(DenseSegWithSkipPSPNet, self).__init__()
-		# self.computing_device = computing_device 
-		self.num_classes = num_classes
-		self.img_channels = img_channels
+    def __init__(self, num_classes=15, img_channels=3, sizes=(1, 2, 3, 6), psp_size=512):
+        super(DenseSegWithSkipPSPNet, self).__init__()
+        # self.computing_device = computing_device 
+        self.num_classes = num_classes
+        self.img_channels = img_channels
 
-		self.down1 = segnetDown2(self.img_channels, 64)
-		self.down2 = segnetDown2(64, 128)
-		self.down3 = segnetDown3(128, 256)
-		self.down4 = segnetDown3(256, 512)
-		self.down5 = segnetDown3(512, 512)
+        self.down1 = segnetDown2(self.img_channels, 64)
+        self.down2 = segnetDown2(64, 128)
+        self.down3 = segnetDown3(128, 256)
+        self.down4 = segnetDown3(256, 512)
+        self.down5 = segnetDown3(512, 512)
 
-		self.psp = PSPModule(psp_size, 512, sizes)
+        self.psp = PSPModule(psp_size, 512, sizes)
         self.drop_1 = nn.Dropout2d(p=0.3)
 
         self.up_1 = PSPUpsample(512, 256)
@@ -183,28 +183,28 @@ class DenseSegWithSkipPSPNet(nn.Module):
             nn.LogSoftmax()
         )
 
-		# self.up5 = segnetUp3(512, 512)
-		# self.up4 = segnetUp3(512, 256)
-		# self.up3 = segnetUp3(256, 128)
-		# self.up2 = segnetUp2(128, 64)
-		# self.up1 = segnetUp1(64, num_classes)
+        # self.up5 = segnetUp3(512, 512)
+        # self.up4 = segnetUp3(512, 256)
+        # self.up3 = segnetUp3(256, 128)
+        # self.up2 = segnetUp2(128, 64)
+        # self.up1 = segnetUp1(64, num_classes)
 
-	def forward(self, inputs):
+    def forward(self, inputs):
 
-		down1, indices_1, unpool_shape1 = self.down1(inputs)
-		down2, indices_2, unpool_shape2 = self.down2(down1)
-		down3, indices_3, unpool_shape3 = self.down3(down2)
-		down4, indices_4, unpool_shape4 = self.down4(down3)
-		down5, indices_5, unpool_shape5 = self.down5(down4)
-		p = self.psp(down5)
+        down1, indices_1, unpool_shape1 = self.down1(inputs)
+        down2, indices_2, unpool_shape2 = self.down2(down1)
+        down3, indices_3, unpool_shape3 = self.down3(down2)
+        down4, indices_4, unpool_shape4 = self.down4(down3)
+        down5, indices_5, unpool_shape5 = self.down5(down4)
+        p = self.psp(down5)
 
-		# up5 = self.up5(p, indices_5, unpool_shape5, down4)
-		# up5 = self.up5(down5, indices_5, unpool_shape5, down4)
-		# up4 = self.up4(up5, indices_4, unpool_shape4, down3)
-		# up3 = self.up3(up4, indices_3, unpool_shape3, down2)
-		# up2 = self.up2(up3, indices_2, unpool_shape2, down1)
-		# up1 = self.up1(up2, indices_1, unpool_shape1)
-		p = self.drop_1(p)
+        # up5 = self.up5(p, indices_5, unpool_shape5, down4)
+        # up5 = self.up5(down5, indices_5, unpool_shape5, down4)
+        # up4 = self.up4(up5, indices_4, unpool_shape4, down3)
+        # up3 = self.up3(up4, indices_3, unpool_shape3, down2)
+        # up2 = self.up2(up3, indices_2, unpool_shape2, down1)
+        # up1 = self.up1(up2, indices_1, unpool_shape1)
+        p = self.drop_1(p)
 
         p = self.up_1(p)
         p = self.drop_2(p)
@@ -215,38 +215,38 @@ class DenseSegWithSkipPSPNet(nn.Module):
         p = self.up_3(p)
         p = self.drop_2(p)
         p = self.final(p)
-		return p
+        return p
 
-	def init_vgg16_params(self, vgg16):
-		blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
+    def init_vgg16_params(self, vgg16):
+        blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
 
-		features = list(vgg16.features.children())
+        features = list(vgg16.features.children())
 
-		vgg_layers = []
-		for _layer in features:
-			if isinstance(_layer, nn.Conv2d):
-				vgg_layers.append(_layer)
+        vgg_layers = []
+        for _layer in features:
+            if isinstance(_layer, nn.Conv2d):
+                vgg_layers.append(_layer)
 
-		merged_layers = []
-		for idx, conv_block in enumerate(blocks):
-			if idx < 2:
-				units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
-			else:
-				units = [
-					conv_block.conv1.cbr_unit,
-					conv_block.conv2.cbr_unit,
-					conv_block.conv3.cbr_unit,
-				]
-			for _unit in units:
-				for _layer in _unit:
-					if isinstance(_layer, nn.Conv2d):
-						merged_layers.append(_layer)
+        merged_layers = []
+        for idx, conv_block in enumerate(blocks):
+            if idx < 2:
+                units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
+            else:
+                units = [
+                    conv_block.conv1.cbr_unit,
+                    conv_block.conv2.cbr_unit,
+                    conv_block.conv3.cbr_unit,
+                ]
+            for _unit in units:
+                for _layer in _unit:
+                    if isinstance(_layer, nn.Conv2d):
+                        merged_layers.append(_layer)
 
-		assert len(vgg_layers) == len(merged_layers)
+        assert len(vgg_layers) == len(merged_layers)
 
-		for l1, l2 in zip(vgg_layers, merged_layers):
-			if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-				assert l1.weight.size() == l2.weight.size()
-				assert l1.bias.size() == l2.bias.size()
-				l2.weight.data = l1.weight.data
-				l2.bias.data = l1.bias.data
+        for l1, l2 in zip(vgg_layers, merged_layers):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                assert l1.weight.size() == l2.weight.size()
+                assert l1.bias.size() == l2.bias.size()
+                l2.weight.data = l1.weight.data
+                l2.bias.data = l1.bias.data
